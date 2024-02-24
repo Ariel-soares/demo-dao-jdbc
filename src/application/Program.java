8c80c18ee1372e32c42f14bf5fa2f8c5be0b4590
Ariel Soares <ariel.sfranco@gmail.com>
2024-02-24 19:35:47 -0300

    FINAL UPDATE

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,22 +10,27 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		Seller seller = sellerDao.findById(3);
-		
+
 		System.out.println(seller);
-		
+
 		System.out.println("============ TESTE 2 ===============");
-		
+
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
-		
-		for(Seller obj : list) {
+
+		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 
+		System.out.println("============ TESTE 3 ===============");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
